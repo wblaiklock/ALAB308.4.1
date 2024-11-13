0e3faaaa06c8fcc64ecba75005f845c886c72e76
wblaiklock <wblaiklock@gmail.com>
2024-11-13 11:42:10 -0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,36 @@
+const users = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      address: {},
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {},
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv",
+      address: {},
+      phone: "010-692-6593 x09125",
+      website: "anastasia.net",
+      company: {},
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+      username: "Samantha",
+      email: "Nathan@yesenia.net",
+      address: {},
+      phone: "1-463-123-4447",
+      website: "ramiro.info",
+      company: {},
+    }
+  ];
+
+  for(let u of users) {
+    console.log(u.name);
+  }
