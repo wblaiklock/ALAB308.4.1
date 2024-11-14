e13cf75dc29dfbaaae849c933a44b725232bc43d
wblaiklock <wblaiklock@gmail.com>
2024-11-14 14:51:50 -0500

    Removed unused page

--- script/script3.js
+++ script/script3.js
@@ -3,7 +3,7 @@
 const str = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26'
 
 let columns = str.split('\n');
-let rows=[];    //2D Array;
+let rows = [];    //2D Array;
 
 for(let i = 0; i < columns.length; i++)
     rows[i] = columns[i].split(',');
@@ -11,7 +11,7 @@ for(let i = 0; i < columns.length; i++)
 
 ////Part 3////
 
-let objects= [];
+let objects = [];
 
 for(let i = 1; i < rows.length; i++)
     objects[i-1] = {id:rows[i][0], name:rows[i][1],occupation:rows[i][2],age:rows[i][3]};
